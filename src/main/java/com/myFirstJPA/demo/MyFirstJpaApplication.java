395be55d69cc883924a19375bf8030ce60d01cd0
manoj1995madushanka <manoj@codegen.net>
2019-08-21 14:24:51 +0530

    first commit

---
+++ src/main/java/com/myFirstJPA/demo/MyFirstJpaApplication.java
@@ -0,0 +1,13 @@
+package com.myFirstJPA.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstJpaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/myFirstJPA/demo/model/Alien.java
@@ -0,0 +1,24 @@
+package com.myFirstJPA.demo.model;
+
+public class Alien {
+	private int aid;
+	private int aname;
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public int getAname() {
+		return aname;
+	}
+	public void setAname(int aname) {
+		this.aname = aname;
+	}
+	@Override
+	public String toString() {
+		return "Alien [aid=" + aid + ", aname=" + aname + "]";
+	}
+	
+
+}
